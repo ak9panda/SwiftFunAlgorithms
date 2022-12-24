b323455243f4541a607e2d508d89cab77a022706
Aung Kyaw Phyoe <akp.aungkyawphyoe7@gmail.com>
2022-12-24 22:59:49 +0630

    roman to integer complete

--- roman_to_integer.playground/Contents.swift
+++ roman_to_integer.playground/Contents.swift
@@ -7,7 +7,7 @@ class Solution {
         let stringArray = Array(s)
         for (index, element) in stringArray.enumerated() {
             if element == "I" {
-                if index != stringArray.count - 1 && (stringArray[index + 1] == "X" || stringArray[index + 1] == "V") {
+                if index != stringArray.count - 1 && (stringArray[index + 1] == "V" || stringArray[index + 1] == "X") {
                     temp = 1
                 }else {
                     result += 1
@@ -16,23 +16,25 @@ class Solution {
                 result += temp != 0 ? 5 - temp : 5
                 temp = 0
             }else if element == "X" {
-                if index != stringArray.count - 1 && stringArray[index + 1] == "C" {
+                if index != stringArray.count - 1 && (stringArray[index + 1] == "L" || stringArray[index + 1] == "C") {
                     temp = 10
                 }else {
                     result += temp != 0 ? 10 - temp : 10
                     temp = 0
                 }
             }else if element == "L" {
-                result += 50
+                result += temp != 0 ? 50 - temp : 50
+                temp = 0
             }else if element == "C" {
-                if index != stringArray.count - 1 && stringArray[index + 1] == "M" {
+                if index != stringArray.count - 1 && (stringArray[index + 1] == "D" || stringArray[index + 1] == "M") {
                     temp = 100
                 }else {
                     result += temp != 0 ? 100 - temp : 100
                     temp = 0
                 }
             }else if element == "D" {
-                result += 500
+                result += temp != 0 ? 500 - temp : 500
+                temp = 0
             }else if element == "M" {
                 result += temp != 0 ? 1000 - temp : 1000
                 temp = 0
